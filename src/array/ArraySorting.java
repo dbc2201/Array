24137b3df9dab66aeb5d1eb4bae5a68ae88ad9bb
dbc2201 <divyanshb22@gmail.com>
2020-01-13 16:14:58 +0530

    eod commit

--- src/array/ArraySorting.java
+++ src/array/ArraySorting.java
@@ -7,8 +7,42 @@
 
 package array;
 
-public class ArraySorting {
-    public void bubbleSort(int[] array) {
+import java.util.Arrays;
 
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        printArray(numbers);
+        initializeArray(numbers);
+        printArray(numbers);
+        bubbleSort(numbers);
+        printArray(numbers);
+    }
+
+    public static void bubbleSort(int[] array) {
+        for (int j = 0; j < array.length - j; j++) {
+            for (int i = 0; i < j; i++) {
+                if (array[i] > array[i + 1]) {
+                    swapArrayValues(i, i + 1, array);
+                }
+            }
+        }
+    }
+
+    private static void swapArrayValues(int i, int i1, int[] array) {
+        int temp = array[i];
+        array[i] = array[i1];
+        array[i1] = temp;
+    }
+
+
+    public static void initializeArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 10 - i;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
     }
 }
